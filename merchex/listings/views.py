b87abd34b6be637550f8184e0f98645f08a4639a
barth-Mboungou <barthmboungou@gmail.com>
2024-11-06 13:04:13 +0100

    create view with urls

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello django</h1>")
+
+def about(request):
+    return HttpResponse("<h1> A propos !</h1> <p>nous adorons le merch</p>")
+
+def listings(request):
+    return HttpResponse("<h1>listing</h1> <p>merch</p>")
+
+def contact(request):
+    return HttpResponse("<h1>contact</h1> <p>merch</p>")
+    
